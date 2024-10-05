b3b04b4969232b74a115273ae58035435a68a3f8
rezaulislam54 <rezaulislam5490@gmail.com>
2024-10-05 23:23:09 +0600

    CRUD oparation post and get oparation added

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hflxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+
+    await client.connect();
+
+    const coffeeCollection = client.db('CofeesDB').collection('coffees');
+   
+    app.get("/coffees", async(req, res)=>{
+        const cursor = coffeeCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get("/coffees/:id", async(req, res)=>{
+        const id = req.prams.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await coffeeCollection.findOne(query)
+        res.send(result);
+    })
+
+
+    app.post("/coffees", async(req, res)=>{
+        const coffee = req.body;
+        const result = await coffeeCollection.insertOne(coffee);
+        res.send(result)
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get("/" ,(req, res)=>{
+    res.send('Coffee Stor Server is Running');
+})
+
+app.listen(port,()=>{
+    console.log(`Coffee Store Server is Running port${port}`)
+})
